f326346b8567cd33f4fe36d9005ec24cfbb55a79
bhanu jangid <bhanujangid@Bhanus-MacBook-Air.local>
2024-08-07 13:28:37 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import InsertionSortVisualization from './components/algorithms/InsertionSortVisualization';
+import SelectionSortVisualization from './components/algorithms/SelectionSortVisualization';
+import BubbleSortVisualization from './components/algorithms/BubbleSortVisualization';
+import MergeSortVisualization from './components/algorithms/MergeSortVisualization';
+import QuickSortVisualization from './components/algorithms/QuickSortVisualization';
+import DFSVisualization from './components/algorithms/DFSVisualization';
+import BFSVisualization from './components/algorithms/BFSVisualization';
+import DijkstraVisualization from './components/algorithms/DijkstraVisualization';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/insertion-sort" element={<InsertionSortVisualization />} />
+                    <Route path="/selection-sort" element={<SelectionSortVisualization />} />
+                    <Route path="/bubble-sort" element={<BubbleSortVisualization />} />
+                    <Route path="/merge-sort" element={<MergeSortVisualization />} />
+                    <Route path="/quick-sort" element={<QuickSortVisualization />} />
+                    <Route path="/dfs" element={<DFSVisualization />} />
+                    <Route path="/bfs" element={<BFSVisualization />} />
+                    <Route path="/dijkstra" element={<DijkstraVisualization />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
